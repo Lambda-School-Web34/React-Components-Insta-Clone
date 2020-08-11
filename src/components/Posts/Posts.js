8c40d531969f67e97897131bcff0a0baf9523d50
alexis villaraza <alexis.borja@gmail.com>
2020-08-11 16:35:32 -0700

    finished comments

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import Post from './Post';
-import './Posts.css';
+import React from "react";
+import Post from "./Post";
+import "./Posts.css";
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
   return (
-    <div className='posts-container-wrapper'>
+    <div className="posts-container-wrapper">
+      {/* {console.log(props.posts)} */}
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map((postInfo, i) => {
+        // console.log(postInfo);
+        return <Post post={postInfo} likePost={likePost} key={i} />
+       
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,19 @@
 // You do not need to change any code in this file for MVP
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icons';
 import "./SearchBar.css";
 
 const SearchBar = (props) => {
+  const { searching } = props
+  const [entry, setEntry] = useState('')
+
+  const handleChange = (event) => {
+    setEntry(event.target.value)
+    searching(entry)
+  }
+
   return (
     <div className="search-bar-wrapper">
       <div className="social">
@@ -15,6 +23,8 @@ const SearchBar = (props) => {
         <input
           type="text"
           placeholder="Search"
+          value={entry}
+          onChange={handleChange}
         />
       </form>
       <div className="social-wrapper">
